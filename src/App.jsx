ff0adc0e1744cd714df96b70d567a6588a0868cb
shillman11 <134095792+shillman11@users.noreply.github.com>
2025-02-08 10:54:20 -0800

    valentines project
    finished the valentines project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
-import lovesvg from "./assets/All You Need Is Love SVG Cut File.svg";
-import lovesvg2 from "./assets/Love In The Air SVG Cut File.svg";
+import { ImHeart } from "react-icons/im";
+import mountain from "./assets/mountain.jpg";
+import halloween from "./assets/halloween.jpg";
+import tree from "./assets/tree.jpg";
+import paris from "./assets/paris.jpg";
+import river from "./assets/river.jpg";
+import hug from "./assets/hug.jpg";
+import duomo from "./assets/duomo.jpg";
+import como from "./assets/como.jpg";
+import desert from "./assets/desert.jpg";
 
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
@@ -41,21 +49,76 @@ export default function Page() {
     <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
+          <div className="z-10 flex-col items-center justify-center ">
+            <div className="flex items-center justify-center space-x-4 pt-10 pb-10">
+              <ImHeart className=" animate-pulseRotate top-10 md:left-24 left-6 md:w-40 w-28 text-red-500 h-40 pl-10" />
+              <div className="text-xl md:text-xl font-bold my-4 mx-auto px-4 md:px-24 text-center w-[70vw] text-outline ">
+                Happy Valentine's Day, my love! Every moment with you feels like
+                a beautiful dream come true. You bring so much joy and warmth
+                into my life, and I’m incredibly grateful for your love and
+                kindness. I’m lucky to have you by my side, and I can’t wait to
+                continue making amazing memories together. I love you!!!!!!!
+              </div>
+              <ImHeart className=" animate-pulseRotate top-10 md:right-24 right-6 md:w-40 w-28 text-red-500 h-40 pr-10" />
+            </div>
+            <div className="flex justify-center">
+              <img
+                className="pb-20"
+                src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+              />{" "}
+            </div>
           </div>
+          <img
+            className="fixed right-[15vw] opacity-80 h-[200px] rounded-lg animate-rotate"
+            src={mountain}
+            alt=""
+          />
+          <img
+            className="fixed left-[15vw] opacity-80 h-[200px] rounded-lg animate-rotate"
+            src={halloween}
+            alt=""
+          />
+          <img
+            className="fixed top-[70vh] left-[10vw] opacity-80 h-[200px] rounded-lg animate-rotate"
+            src={duomo}
+            alt=""
+          />
+          <img
+            className="fixed top-[60vh] right-[10vw] opacity-80 h-[200px] rounded-lg animate-rotate"
+            src={hug}
+            alt=""
+          />
+          <img
+            className="fixed top-[10vh] right-[50vw] opacity-70 h-[200px] rounded-lg animate-rotate"
+            src={desert}
+            alt=""
+          />
+          <img
+            className="fixed top-[80vh] right-[40vw] opacity-80 h-[200px] rounded-lg animate-rotate"
+            src={paris}
+            alt=""
+          />
+          <img
+            className="fixed top-[0vh] right-[0vw] opacity-80 h-[200px] rounded-lg animate-rotate"
+            src={tree}
+            alt=""
+          />
+          <img
+            className="fixed top-[0vh] left-[0vw] opacity-80 h-[200px] rounded-lg animate-rotate"
+            src={como}
+            alt=""
+          />
+          {/* <img
+            className="fixed bottom-[0vh] right-[0vw] opacity-80 h-[200px] rounded-lg animate-rotate"
+            src={desert}
+            alt=""
+          /> */}
         </>
       ) : (
         <>
-          <img
-            src={lovesvg}
-            className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
-          />
-          <img
-            src={lovesvg2}
-            className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
-          />
+          <ImHeart className="fixed animate-pulseRotate top-10 md:left-24 left-6 md:w-40 w-28 text-red-500 h-40" />
+          <ImHeart className="fixed bottom-16 -z-10 animate-pulseRotateDelay md:right-24 right-10 md:w-40 w-32 text-red-500 h-40" />
+
           <img
             className="h-[230px] rounded-lg shadow-lg"
             src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
@@ -80,22 +143,6 @@ export default function Page() {
           </div>
         </>
       )}
-      <Footer />
     </div>
   );
 }
-
-const Footer = () => {
-  return (
-    <a
-      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
-    >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
-    </a>
-  );
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulseRotate: "pulseRotate 3s ease-in-out infinite", // Custom pulseRotate animation
+        pulseRotateDelay: "pulseRotateDelay 3s ease-in-out infinite", // Custom pulseRotate animation
+        rotate: "rotate 3s ease-in-out infinite",
+        rotateDelay: "rotateDelay 3s ease-in-out infinite",
+      },
+
+      keyframes: {
+        pulseRotate: {
+          "0%, 100%": {
+            transform: "rotate(-15deg)", // Start rotating to the left
+            opacity: 0.5, // Transparency starts at 50%
+          },
+          "50%": {
+            transform: "rotate(15deg)", // Rotate to the right
+            opacity: 1, // Transparency ends at 100%
+          },
+        },
+
+        pulseRotateDelay: {
+          "0%, 100%": {
+            transform: "rotate(15deg)", // Start rotating to the left
+            opacity: 1, // Transparency starts at 50%
+          },
+          "50%": {
+            transform: "rotate(-15deg)", // Rotate to the right
+            opacity: 0.5, // Transparency ends at 100%
+          },
+        },
+
+        rotate: {
+          "0%, 100%": {
+            transform: "rotate(1deg)", // Start rotating to the left
+          },
+          "50%": {
+            transform: "rotate(-1deg)", // Rotate to the right
+          },
+        },
+
+        rotateDelay: {
+          "0%, 100%": {
+            transform: "rotate(-1deg)", // Start rotating to the left
+          },
+          "50%": {
+            transform: "rotate(1deg)", // Rotate to the right
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
